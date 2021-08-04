ea154de4ccbd36863afee00d82ef9cdb8abacc80
saksham2008 <84182433+saksham2008@users.noreply.github.com>
2021-08-04 12:55:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -86,11 +86,13 @@ function draw() {
 function keyPressed() {
 	 if(keyCode === LEFT_ARROW){
 		helicopterSprite.x=helicopterSprite.x-20;
-		translation={x:-20,y:0} Matter.Body.translate(packageBody, translation)
+		translation={x:-20,y:0} 
+		Matter.Body.translate(packageBody, translation)
 	  }
 	   else if (keyCode === RIGHT_ARROW){
 		helicopterSprite.x=helicopterSprite.x+20;
-		translation={x:20,y:0} Matter.Body.translate(packageBody, translation) } 
+		translation={x:20,y:0}
+		Matter.Body.translate(packageBody, translation) } 
 	   else if (keyCode === DOWN_ARROW){
 		Matter.Body.setStatic(packageBody,false);
 	   }
